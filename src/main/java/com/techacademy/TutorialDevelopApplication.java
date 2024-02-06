a58312eb5a91edca338a21a3a97e91b4de616e87
Horiuchi8 <147533337+Horiuchi8@users.noreply.github.com>
2024-02-06 16:17:11 +0900

    ユーザー登録機能と更新機能を追加
    Add:register.html&css/update.html&css
    Improve:list.html/UserController/UserService

--- src/main/java/com/techacademy/TutorialDevelopApplication.java
+++ src/main/java/com/techacademy/TutorialDevelopApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TutorialDevelopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TutorialDevelopApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialDevelopApplication.class, args);
+    }
 }
